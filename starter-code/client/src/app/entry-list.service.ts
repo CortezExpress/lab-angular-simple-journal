3ff228d6b6aad25cc4a3097bff207309ba16ce06
MARTIN GENET <martingenet@MHG.local>
2018-03-09 10:04:36 -0500

    making sure this works

---
+++ starter-code/client/src/app/entry-list.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class EntryListService {
+  title: String;
+  date: Date;
+
+  constructor() { }
+
+  displayTitle(){
+
+  }
+
+
+}
+
+
+
